9a7995f5778d41cfa864be848975c10f3a7bb3bf
bitbuc13 <s13silvia13@gmail.com>
2019-02-24 22:57:25 +0900

    [追加]画面外に出たアイテムを破棄する機能を追加
    Unityちゃんの後方に一定距離を保ちUnityちゃんを追尾する壁を配置した
    壁はZ方向にのみ移動する
    特定のTagのオブジェクトが壁と接触すると破棄される

---
+++ Assets/DestroyUnnecessaryObjects.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyUnnecessaryObjects : MonoBehaviour {
+
+    //UnityChanのゲームオブジェクト
+    private GameObject unityChan;
+
+    // Use this for initialization
+    void Start () {
+
+        //シーン中unityChanオブジェクトを取得
+        this.unityChan = GameObject.Find("unitychan");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //unityChanから一定の距離に移動
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.unityChan.transform.position.z - 7.5f);
+
+    }
+
+    //トリガーモードで他のオブジェクトと接触した場合の処理
+    private void OnTriggerEnter(Collider other)
+    {
+        //接触したオブジェクトを削除
+        if(other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag" || other.gameObject.tag == "CoinTag")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
